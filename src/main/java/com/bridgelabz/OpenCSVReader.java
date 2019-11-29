fa18ecee322f8023ad23920eb647ebd7ce3b2321
GaikwadRavi03 <gaikwadr576@gmail.com>
2019-11-29 14:56:41 -0500

    Add: Read CSV File into String Array

--- src/main/java/com/bridgelabz/OpenCSVReader.java
+++ src/main/java/com/bridgelabz/OpenCSVReader.java
@@ -1,5 +1,30 @@
 package com.bridgelabz;
 
-public class OpenCSVReader {
 
+import com.opencsv.CSVReader;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class OpenCSVReader {
+    private static final String SAMPLE_CSV_FILE_PATH = "/home/admin141/IdeaProjects/openCSV/src/main/resources/user.csv";
+
+    public static void main(String[] args) throws IOException {
+        try (
+                Reader reader = Files.newBufferedReader(Paths.get(SAMPLE_CSV_FILE_PATH));
+                CSVReader csvReader = new CSVReader(reader);
+        ) {
+            // Reading Records One by One in a String array
+            String[] nextRecord;
+            while ((nextRecord = csvReader.readNext()) != null) {
+                System.out.println("Name : " + nextRecord[0]);
+                System.out.println("Email : " + nextRecord[1]);
+                System.out.println("Phone : " + nextRecord[2]);
+                System.out.println("Country : " + nextRecord[3]);
+                System.out.println("==========================");
+            }
+        }
+    }
 }
