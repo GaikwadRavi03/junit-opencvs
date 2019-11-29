fb23e016d544afef344b83d8c29036fd75c30a90
GaikwadRavi03 <gaikwadr576@gmail.com>
2019-11-29 15:00:11 -0500

    Add: Read All Records at Once

--- src/main/java/com/bridgelabz/OpenCSVReader.java
+++ src/main/java/com/bridgelabz/OpenCSVReader.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 
 public class OpenCSVReader {
     private static final String SAMPLE_CSV_FILE_PATH = "/home/admin141/IdeaProjects/openCSV/src/main/resources/user.csv";
@@ -18,12 +19,21 @@ public class OpenCSVReader {
         ) {
             // Reading Records One by One in a String array
             String[] nextRecord;
-            while ((nextRecord = csvReader.readNext()) != null) {
+            /*while ((nextRecord = csvReader.readNext()) != null) {
                 System.out.println("Name : " + nextRecord[0]);
                 System.out.println("Email : " + nextRecord[1]);
                 System.out.println("Phone : " + nextRecord[2]);
                 System.out.println("Country : " + nextRecord[3]);
                 System.out.println("==========================");
+            }*/
+
+            List<String[]> records = csvReader.readAll();
+            for (String[] record : records) {
+                System.out.println("Name : " + record[0]);
+                System.out.println("Email : " + record[1]);
+                System.out.println("Phone : " + record[2]);
+                System.out.println("Country : " + record[3]);
+                System.out.println("---------------------------");
             }
         }
     }
