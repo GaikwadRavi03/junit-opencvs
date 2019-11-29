7604e46b733b218b1c5561c50f25acc2d4862e59
GaikwadRavi03 <gaikwadr576@gmail.com>
2019-11-29 15:15:40 -0500

    Add: Read into Memory not suitable for large CSV

--- src/main/java/com/bridgelabz/OpenCSVReadAndParseToBean.java
+++ src/main/java/com/bridgelabz/OpenCSVReadAndParseToBean.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Iterator;
+import java.util.List;
 
 public class OpenCSVReadAndParseToBean {
     private static final String SAMPLE_CSV_FILE_PATH = "/home/admin141/IdeaProjects/openCSV/src/main/resources/user.csv";
@@ -21,8 +21,7 @@ public class OpenCSVReadAndParseToBean {
                     .withIgnoreLeadingWhiteSpace(true)
                     .build();
 
-            Iterator<CSVUser> csvUserIterator = csvToBean.iterator();
-
+            /*Iterator<CSVUser> csvUserIterator = csvToBean.iterator();
             while (csvUserIterator.hasNext()) {
                 CSVUser csvUser = csvUserIterator.next();
                 System.out.println("Name : " + csvUser.getName());
@@ -30,7 +29,18 @@ public class OpenCSVReadAndParseToBean {
                 System.out.println("PhoneNo : " + csvUser.getPhoneNo());
                 System.out.println("Country : " + csvUser.getCountry());
                 System.out.println("==========================");
+            }*/
+            // Reads all CSV contents into memory (Not suitable for large CSV files)
+            List<CSVUser> csvUsers = csvToBean.parse();
+
+            for (CSVUser csvUser : csvUsers) {
+                System.out.println("Name : " + csvUser.getName());
+                System.out.println("Email : " + csvUser.getEmail());
+                System.out.println("PhoneNo : " + csvUser.getPhoneNo());
+                System.out.println("Country : " + csvUser.getCountry());
+                System.out.println("==========================");
             }
+
         }
     }
 }
